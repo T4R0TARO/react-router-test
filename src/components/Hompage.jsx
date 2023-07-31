7c17521dbcce8e775b7df93e473c6b05d7629f23
josh manansala <joshmanansalagsd@gmail.com>
2023-07-30 17:20:51 -0700

    add: content to homepage

--- src/components/Hompage.jsx
+++ src/components/Hompage.jsx
@@ -12,6 +12,12 @@ function Homepage() {
         commodi laborum eius ullam libero itaque, sed fugit repellat, nulla
         officia deserunt eveniet, similique odit adipisci amet quas totam!
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde facere
+        inventore voluptates perspiciatis quas modi aut eaque quod? Repellendus
+        qui quam quo cumque repudiandae ipsum impedit dolores nemo suscipit
+        doloremque!
+      </p>
     </div>
   );
 }
